73d11c9a78ac75259445ebb72c815921359b1d71
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2025-06-13 10:42:12 +0100

    Some hard-coded links have gone stale

--- src/ui/assets/sources/ArchitectureKitSource.js
+++ src/ui/assets/sources/ArchitectureKitSource.js
@@ -5,7 +5,7 @@ export default class ArchitectureKitSource extends KitSource {
   constructor(api) {
     super(
       api,
-      "https://assets-prod.reticulum.io/kits/architecture/ArchKit-64274f78e194a993850e208cbaa2fe7c5a35a955.gltf"
+      "https://avnfs.com/Z_4qFCwNt4xDjSrhoiKtIiS9_WoqDBBTTIXWkSTHmyo?size=1110826&type=model%2Fgltf%2Bjson&name=ArchKit-64274f78e194a993850e208cbaa2fe7c5a35a955.gltf"
     );
     this.id = "architecture-kit";
     this.name = "Architecture Kit";
--- src/ui/assets/sources/RockKitSource.js
+++ src/ui/assets/sources/RockKitSource.js
@@ -3,7 +3,7 @@ import { TransformPivot } from "../../../editor/controls/SpokeControls";
 
 export default class RockKitSource extends KitSource {
   constructor(api) {
-    super(api, "https://assets-prod.reticulum.io/kits/rock/RocksKit_v2-f259c85a5e6091bd72eac7e2fdca177209613f1b.gltf");
+    super(api, "https://avnfs.com/wpkcAq_TKHbYrH3h0OOip61UX6xOFk_QYP3DTTTGQf0?size=13534&type=model%2Fgltf%2Bjson&name=RocksKit_v2-f259c85a5e6091bd72eac7e2fdca177209613f1b.gltf");
     this.id = "rock-kit";
     this.name = "Rock Kit";
     this.transformPivot = TransformPivot.Selection;
